972893798c9357ea0e6337f87ad351136a202015
Fujino N's <67798965+FujinoNs@users.noreply.github.com>
2021-06-06 18:53:58 +0700

    Add files via upload

---
+++ src/Fujino.Network.Info.API/Fujino.Network.Info.API/Json_Modul.cs
@@ -0,0 +1,14 @@
+﻿namespace FujinoNs.NetworkInfo
+{
+    class Json_Modul
+    {
+        public string status { get; set; }
+        public string country { get; set; }
+        public string countryCode { get; set; }
+        public string regionName { get; set; }
+        public string city { get; set; }
+        public string timezone { get; set; }
+        public string isp { get; set; }
+        public string query { get; set; }
+    }
+}
---
+++ src/Fujino.Network.Info.API/Fujino.Network.Info.API/KC_NetworkInfo.cs
@@ -0,0 +1,47 @@
+﻿namespace FujinoNs.NetworkInfo
+{
+    public class KC_NetworkInfo
+    {
+        Json_Modul _Modul;
+        public KC_NetworkInfo()
+        {
+            _Modul = Newtonsoft.Json.JsonConvert.DeserializeObject<Json_Modul>(new System.Net.WebClient().DownloadString("http://ip-api.com/json/"));
+        }
+
+        public string Status()
+        {
+            return _Modul.status;
+        }
+
+        public string Country()
+        {
+            return _Modul.country + " (" + _Modul.countryCode + ")";
+        }
+
+        public string RegionName()
+        {
+            return _Modul.regionName;
+        }
+
+        public string City()
+        {
+            return _Modul.city;
+        }
+
+        public string TimeZone()
+        {
+            return _Modul.timezone;
+        }
+
+        public string ISP()
+        {
+            return _Modul.isp;
+        }
+
+        public string IPAddress()
+        {
+            return _Modul.query;
+        }
+    }
+
+}
